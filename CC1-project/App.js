d30232da926d8b71b1ca67a5ef890ce7c3eb0dad
sneham29 <153273230+sneham29@users.noreply.github.com>
2023-12-14 09:33:26 +0530

    Add files via upload

---
+++ CC1-project/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ToyStoreRegistrationForm from './components/ToyStoreRegistrationForm.jsx';
+
+
+function App() {
+  return (
+    <div className="App">
+      <ToyStoreRegistrationForm/>
+    </div>
+  );
+}
+
+export default App;
